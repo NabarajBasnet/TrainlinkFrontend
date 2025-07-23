4212ddaafa93b25df97b55f2305e1714c9c57164
Nabaraj Basnet <nabarajbasnet2000@gmail.com>
2025-07-23 21:05:36 +0545

    UI: installed required shad cn components and some configs

---
+++ src/app/clientLayout.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from "react";
+
+type ClientLayoutProps = {
+    children: React.ReactNode
+}
+
+const ClientLayout = ({ children }: ClientLayoutProps) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
+
+export default ClientLayout;
